ec53656642920bd2188cd15f081ee1057a22beb0
KKFinger <791173697@qq.com>
2017-08-11 21:43:56 +0800

    Initial Commit

---
+++ KKPlateGirdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKPlateGirdView
+//
+//  Created by finger on 2017/8/11.
+//  Copyright © 2017年 finger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKPlateGirdView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKPlateGirdView
+//
+//  Created by finger on 2017/8/11.
+//  Copyright © 2017年 finger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
